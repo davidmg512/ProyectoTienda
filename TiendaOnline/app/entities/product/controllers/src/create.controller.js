81e5bb075f20fa94b4c294eb71cd0db1d5870dc9
davidmg512 <davidmgaspe@gmail.com>
2024-09-10 18:08:48 +0200

    cambios

--- TiendaOnline/app/entities/product/controllers/src/create.controller.js
+++ TiendaOnline/app/entities/product/controllers/src/create.controller.js
@@ -3,39 +3,13 @@ const LogService = require("@services/log.service");
 const DbService = require("@services/db.service");
 const { ExceptionHandler } = require('kainda');
 //const cloudflare = require('cloudflare-api');
-const fetch = require('node-fetch').default;
+//const fetch = require('node-fetch').default;
 const FormData = require('form-data');
 const fs = require('fs')
-/*
-const cf = cloudflare({
-    email: 'susojeruso2000@gmail.com',
-    token: '316430a42c83770acd2388043d75ca80b8169'
-});
-*/
-/**
- * Create new product
- * @async
- * @param {Express.Request} req
- * @param {Express.Response} res
- * @returns {void}
- */
-/*async function createProduct(req, res) {
-    const Product = ModelsService.Models.Product;
-    let transaction = await Product.transaction(DbService.get());
-    try {
-        const product = await Product.Controller.__createProduct(req.body, { transaction });
-        await transaction.commit();
-        return res.status(201).json(product.toJSON());
-    } catch (error) {
-        LogService.ErrorLogger.error(error);
-        if (transaction) {
-            await transaction.rollback();
-        }
-        ExceptionHandler(error, res);
-    }
-}*/
+
 
   
+
 
 async function createProduct(req, res){
     
@@ -47,15 +21,16 @@ async function createProduct(req, res){
     try{
 
     const {producto_nombre, producto_descripcion, producto_precio, producto_categoria} = req.body;
-    const imageUrls = [];
-    const imageFile = req.body.producto_imagenes.nombre;
-    const imageStream = fs.readFile(imageFile.path);
+    //const imageUrls = [];
+    //const imageFile = req.body.producto_imagenes.nombre;
+    //const imageStream = fs.readFile(imageFile.path);
 
-    const cloudflareApiToken = "316430a42c83770acd2388043d75ca80b8169";
+    //const cloudflareApiToken = "316430a42c83770acd2388043d75ca80b8169";
     
-    const cloudflarePostBody = new FormData();
-    cloudflarePostBody.append("file", imageStream);
+    //const cloudflarePostBody = new FormData();
+    //cloudflarePostBody.append("file", imageStream);
     
+    /*
     const cloudflareResponse = await fetch("https://api.cloudflare.com/client/v4/accounts/7487b224e6bdb241146084a2bd8da49d/images/v1", {
         method: "POST",
         headers: {
@@ -63,10 +38,10 @@ async function createProduct(req, res){
             "Content-Type": "multipart/form-data"
         },
         body: cloudflarePostBody,
-    });
+    });*/
 
-    const cloudflareResponseBody = await cloudflareResponse.json();
-    const imageUrl = cloudflareResponseBody.result.url;
+    //const cloudflareResponseBody = await cloudflareResponse.json();
+    const imageUrl = "producto_1.jpg"
 
     const newProduct = new Product({
         producto_nombre,
